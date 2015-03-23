89447ffc1bb0899a80b2cf965203fa19f7cd91e7
Ryan Peden <ryan04@gmail.com>
2015-03-22 20:45:00 -0400

    finished off parenthesis stack client. tests pass.

--- Algorithms/Parentheses.cs
+++ Algorithms/Parentheses.cs
@@ -10,10 +10,41 @@ namespace Algorithms
     //use a stack to determine if parentheses are correctly balanced
     public class Parentheses
     {
+        private const char LEFT_PAREN = '(';
+        private const char RIGHT_PAREN = ')';
+
+        private const char LEFT_SQUARE = '[';
+        private const char RIGHT_SQUARE = ']';
+
+        private const char LEFT_CURLY = '{';
+        private const char RIGHT_CURLY = '}';
+
         public static bool CheckIfBalanced(string parString)
         {
-            throw new NotImplementedException();
-            return false;
+            if (parString.Length == 0) return true;
+            var stack = new LinkedStack<char>();
+
+            foreach(char c in parString)
+            {
+                switch(c)
+                {
+                    case LEFT_PAREN:
+                    case LEFT_SQUARE:
+                    case LEFT_CURLY:
+                        stack.Push(c);
+                        break;
+                    case RIGHT_PAREN:
+                        if (stack.Size == 0 || stack.Pop() != LEFT_PAREN) return false;
+                        break;
+                    case RIGHT_SQUARE:
+                        if (stack.Size == 0 || stack.Pop() != LEFT_SQUARE) return false;
+                        break;
+                    case RIGHT_CURLY:
+                        if (stack.Size == 0 || stack.Pop() != LEFT_CURLY) return false;
+                        break;
+                }
+            }
+            return true;
         }
     }
 
@@ -31,7 +62,7 @@ namespace Algorithms
         public void TestUnbalancedParentheses()
         {
             var testString = "[(])";
-            Assert.AreEqual(true, Parentheses.CheckIfBalanced(testString));
+            Assert.AreEqual(false, Parentheses.CheckIfBalanced(testString));
         }
     }
 }
