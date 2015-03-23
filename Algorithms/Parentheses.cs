74aa5866195bf6569bbdf0a9413064c858fd93cb
Ryan Peden <ryan04@gmail.com>
2015-03-22 20:30:35 -0400

    added balanced skeleton parentheses checked and tests

---
+++ Algorithms/Parentheses.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    //use a stack to determine if parentheses are correctly balanced
+    public class Parentheses
+    {
+        public static bool CheckIfBalanced(string parString)
+        {
+            throw new NotImplementedException();
+            return false;
+        }
+    }
+
+    [TestFixture]
+    public class ParenthesesTest
+    {
+        [Test]
+        public void TestBalancedParentheses()
+        {
+            var testString = "[()]{}{[()()]()}";
+            Assert.AreEqual(true, Parentheses.CheckIfBalanced(testString));
+        }
+
+        [Test]
+        public void TestUnbalancedParentheses()
+        {
+            var testString = "[(])";
+            Assert.AreEqual(true, Parentheses.CheckIfBalanced(testString));
+        }
+    }
+}
